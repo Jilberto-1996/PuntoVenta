9812e7eeda133d3de1d489da43f53197d3526e1c
Jilberto <nmvaquez94@gmail.com>
2019-11-08 18:42:19 -0600

    Soy el primer usuario que creo la carpeta pero ya lo corregi

--- ejemplo1.c
+++ ejemplo1.c
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
+
 hola soy yo de nuevo por tercera vez aqui haciendo la prueba
-=======
-hola mundo a todos saludos
 
-
-bienvenidos como estan 
-
-bla bla bla bla bla bla
-Hola mundo de nuevo
-
->>>>>>> e51601c7cfa449f471330b9244671a497edd8749
+Hola soy el primer usuario que creo el archivo de este curso
